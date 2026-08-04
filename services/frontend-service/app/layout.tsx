415be3008e1827484f514d8f868e367742f5e795
athulraj2027 <athulraj2027@gmail.com>
2026-08-04 09:29:41 +0530

    init: tanstack-query

--- services/frontend-service/app/layout.tsx
+++ services/frontend-service/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import QueryProvider from "@/components/providers/QueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -47,7 +48,9 @@ export default function RootLayout({
         />
       </head>
       <body className="min-h-full flex flex-col bg-background text-foreground transition-colors duration-300">
-        <main className="flex-1 flex flex-col">{children}</main>
+        <QueryProvider>
+          <main className="flex-1 flex flex-col">{children}</main>
+        </QueryProvider>
       </body>
     </html>
   );
